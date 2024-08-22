91ca9b28efcfad436be4c8979767d8f7cd55aa8e
Cristian zvdy <cristian.030902@gmail.com>
2024-08-22 20:43:36 +0200

    [feat] Tokyo Night theme palette

--- main.py
+++ main.py
@@ -4,17 +4,31 @@ from ImageGoNord import GoNord
 
 go_nord = GoNord()
 go_nord.reset_palette()
-go_nord.add_color_to_palette('#1a1b26')
-go_nord.add_color_to_palette('#24283b')
-go_nord.add_color_to_palette('#cfc9c2')
-go_nord.add_color_to_palette('#565f89')
-go_nord.add_color_to_palette('#bb9af7')
-go_nord.add_color_to_palette('#7dcfff')
-go_nord.add_color_to_palette('#9ece6a')
-go_nord.add_color_to_palette('#e0af68')
-go_nord.add_color_to_palette('#f7768e')
-go_nord.add_color_to_palette('#8c4351')
-go_nord.add_color_to_palette('#ff9e64')
+go_nord.add_color_to_palette('#1f2335')  # Background
+go_nord.add_color_to_palette('#24283b')  # Current Line
+go_nord.add_color_to_palette('#292e42')  # Foreground
+go_nord.add_color_to_palette('#3b4261')  # Comment
+go_nord.add_color_to_palette('#414868')  # Darker Comment
+go_nord.add_color_to_palette('#545c7e')  # Darker Foreground
+go_nord.add_color_to_palette('#565f89')  # Lighter Comment
+go_nord.add_color_to_palette('#737aa2')  # Lighter Foreground
+go_nord.add_color_to_palette('#a9b1d6')  # Light Blue
+go_nord.add_color_to_palette('#c0caf5')  # Lightest Blue
+go_nord.add_color_to_palette('#394b70')  # Dark Blue
+go_nord.add_color_to_palette('#3d59a1')  # Blue
+go_nord.add_color_to_palette('#7aa2f7')  # Light Blue
+go_nord.add_color_to_palette('#7dcfff')  # Cyan
+go_nord.add_color_to_palette('#b4f9f8')  # Light Cyan
+go_nord.add_color_to_palette('#bb9af7')  # Magenta
+go_nord.add_color_to_palette('#9d7cd8')  # Light Magenta
+go_nord.add_color_to_palette('#ff9e64')  # Orange
+go_nord.add_color_to_palette('#ffc777')  # Light Orange
+go_nord.add_color_to_palette('#c3e88d')  # Green
+go_nord.add_color_to_palette('#4fd6be')  # Light Green
+go_nord.add_color_to_palette('#41a6b5')  # Dark Cyan
+go_nord.add_color_to_palette('#ff757f')  # Red
+go_nord.add_color_to_palette('#c53b53')  # Dark Red
+go_nord.add_color_to_palette('#ff007c')  # Pink
 
 app = Flask(__name__)
 
